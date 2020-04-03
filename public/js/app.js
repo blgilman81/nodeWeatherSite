e751cafac2e52cca9afc9ee6b58df9c691b15ba9
roshambeaux <blgilman81@gmail.com>
2020-04-02 23:14:53 -0500

    remove comments from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-// fetch('http://localhost:3000/weather?address=minneapolis').then(response => {
-//         response.json().then(data => {
-//                 if (data.error) {
-//                         console.log(data.error);
-//                 } else {
-//                         console.log(data);
-//                 }
-//         });
-// });
-
 const form = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#messageOne');
